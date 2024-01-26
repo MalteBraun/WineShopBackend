ea3f8fb8ede8b9d62b928a0242c6dad417f35363
Malte <Maltebraun@outlook.com>
2024-01-26 16:51:15 +0100

    initial setup

---
+++ src/main/java/de/malte/wineshopbackend/WineShopBackendApplication.java
@@ -0,0 +1,13 @@
+package de.malte.wineshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineShopBackendApplication.class, args);
+    }
+
+}
